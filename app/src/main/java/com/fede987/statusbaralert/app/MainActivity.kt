cdf2d91dde581cf088af9ee1d63ecb4d3a32162c
Federico Marin <federico.marin@bandyer.com>
2018-09-08 11:05:43 +0200

    Added new features, update text and progressbar visibility, custom typeface

--- app/src/main/java/com/fede987/statusbaralert/app/MainActivity.kt
+++ app/src/main/java/com/fede987/statusbaralert/app/MainActivity.kt
@@ -1,51 +1,100 @@
 package com.fede987.statusbaralert.app
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.fede987.statusbaralert.StatusBarAlert
+import com.fede987.statusbaralert.StatusBarAlertView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var typeface: Typeface? = null
+    val handler: Handler = Handler()
+    var alert1: StatusBarAlertView? = null
+    var alert2: StatusBarAlertView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupButtons()
+        createCustomTypeface()
 
+        setupButtons()
     }
+
+    override fun onDestroy() {
+        alert1 = null
+        alert2 = null
+        handler.removeCallbacksAndMessages(null)
+        StatusBarAlert.hide(this, Runnable{})
+        super.onDestroy()
+    }
+
+    fun createCustomTypeface() {
+        typeface = Typeface.createFromAsset(assets, "font/Lato-Regular.ttf")
+    }
+
 
     fun setupButtons() {
 
         button1.setOnClickListener({
 
-            StatusBarAlert.Builder(this@MainActivity)
+            alert1 = StatusBarAlert.Builder(this@MainActivity)
                     .autoHide(true)
                     .showProgress(true)
+                    .withDuration(10000)
                     .withText("autohide!")
+                    .withTypeface(typeface!!)
                     .withAlertColor(R.color.colorPrimaryDark)
                     .build()
+
+            handler.postDelayed({
+                alert1?.updateText("Phase 1!")
+                alert1?.showIndeterminateProgress()
+            },2000)
+
+
+            handler.postDelayed({
+                alert1?.updateText("Phase 2!")
+                alert1?.showIndeterminateProgress()
+            },4000)
+
+            handler.postDelayed({
+                alert1?.updateText("Completed!")
+                alert1?.hideIndeterminateProgress()
+            },7500)
 
         })
 
         button2.setOnClickListener({
 
-            StatusBarAlert.Builder(this@MainActivity)
+            alert2 = StatusBarAlert.Builder(this@MainActivity)
                     .autoHide(false)
                     .showProgress(false)
                     .withText("RED ALERT!")
+                    .withTypeface(typeface!!)
                     .withAlertColor(R.color.red)
                     .build()
+
+            handler.postDelayed({
+                if(alert2?.parent!=null)
+                    alert2?.updateText("INFO UPDATED!!")
+            },2000)
+
 
         })
 
         button3.setOnClickListener({
 
-            StatusBarAlert.Builder(this@MainActivity)
+            StatusBarAlert.Builder(
+                    this@MainActivity)
                     .autoHide(true)
                     .withDuration(100)
                     .showProgress(false)
                     .withText("BLINK!")
+                    .withTypeface(typeface!!)
                     .withAlertColor(R.color.green)
                     .build()
 
--- status-bar-alert/src/main/java/com/fede987/statusbaralert/utils/ScreenUtils.kt
+++ status-bar-alert/src/main/java/com/fede987/statusbaralert/utils/ScreenUtils.kt
@@ -1,48 +1,50 @@
 package com.fede987.statusbaralert.utils
 
 import android.app.Activity
-import android.content.Context
+import android.os.Build
 import android.util.DisplayMetrics
+import android.view.WindowManager
 
-class ScreenUtils {
+private var titleBarHeigh = 0
+private val dipsMap: MutableMap<Float, Int> = mutableMapOf()
 
-    companion object {
+internal fun Activity.convertDpToPixel(dp: Float): Int {
 
-        private var titleBarHeigh = 0
-        private val dipsMap: MutableMap<Float, Int> = mutableMapOf()
-
-        fun convertDpToPixel(dp: Float, context: Context): Int {
-
-            if (dipsMap.containsKey(dp)) {
-                return dipsMap[dp]!!
-            }
-
-            val resources = context.resources
-            val metrics = resources.displayMetrics
-            val value = (dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
-            dipsMap[dp] = value
-
-            return value
-        }
-
-
-        fun getStatusBarHeight(any: Activity): Int {
-
-            if (titleBarHeigh > 0) {
-                return titleBarHeigh
-            }
-
-            val resourceId = any.resources.getIdentifier("status_bar_height", "dimen", "android")
-            if (resourceId > 0) {
-                titleBarHeigh = any.resources.getDimensionPixelSize(resourceId)
-            } else {
-                titleBarHeigh = convertDpToPixel(25f, any)
-            }
-
-            return titleBarHeigh
-
-        }
-
+    if (dipsMap.containsKey(dp)) {
+        return dipsMap[dp]!!
     }
 
+    val resources = this.resources
+    val metrics = resources.displayMetrics
+    val value = (dp * (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
+    dipsMap[dp] = value
+
+    return value
+}
+
+internal fun Activity.getStatusBarHeight(): Int {
+
+    if (titleBarHeigh > 0) {
+        return titleBarHeigh
+    }
+
+    val resourceId = this.resources.getIdentifier("status_bar_height", "dimen", "android")
+    if (resourceId > 0) {
+        titleBarHeigh = this.resources.getDimensionPixelSize(resourceId)
+    } else {
+        titleBarHeigh = convertDpToPixel(25f)
+    }
+
+    return titleBarHeigh
+}
+
+internal fun Activity.isTranslucentStatusBar(): Boolean {
+    val w = this.window
+    val lp = w.attributes
+    val flags = lp.flags
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS == WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+    } else {
+        return false
+    }
 }
